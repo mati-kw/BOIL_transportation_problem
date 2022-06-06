ba9080ef6b968e23aba0ffe8c20841ca88f81f57
mati-kw <mati.kwiecien@gmail.com>
2022-06-06 19:23:27 +0200

    tables styling

--- boil_app/src/tables/RecipientTab.js
+++ boil_app/src/tables/RecipientTab.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 export default function recipientTab() {
   return (
-    <div>
-        <p1> this is place for recipient tab</p1>
-        <p1>tab here</p1>
+    <div class="w-25 p-3">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Recipient 1</th>
+            <th scope="col">Recipient 2</th>
+            <th scope="col">Recipient 3</th>
+          </tr>
+        </thead>
+          <tr>
+              <th scope="row">Demand:</th>
+              <td><input class="demand border-0 " type="number" placeholder="0"/></td>
+              <td><input class="demand border-0" type="number" placeholder="0"/></td>
+              <td><input class="demand border-0" type="number" placeholder="0"/></td>
+          </tr>
+          <tr>
+              <th scope="row">Selling price:</th>
+              <td><input class="sellingPrice border-0" type="number" placeholder="0"/></td>
+              <td><input class="sellingPrice border-0" type="number" placeholder="0"/></td>
+              <td><input class="sellingPrice border-0" type="number" placeholder="0"/></td>
+          </tr>
+      </table>
     </div>
     
   )
--- boil_app/src/tables/SupplierTab.js
+++ boil_app/src/tables/SupplierTab.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 export default function supplierTab() {
   return (
-    <div>
-        <p1> this is place for supplier tab</p1>
-        <p1>tab here</p1>
+    <div class="w-25 p-3">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Supplier 1</th>
+            <th scope="col">Supplier 2</th>
+            <th scope="col">Supplier 3</th>
+          </tr>
+        </thead>
+          <tr>
+              <th scope="row">Supply:</th>
+              <td><input class="supply border-0" type="number" placeholder="0"/></td>
+              <td><input class="supply border-0" type="number" placeholder="0"/></td>
+              <td><input class="supply border-0" type="number" placeholder="0"/></td>
+          </tr>
+          <tr>
+              <th scope="row">Purchase cost:</th>
+              <td><input class="purchaseCost border-0" type="number" placeholder="0"/></td>
+              <td><input class="purchaseCost border-0" type="number" placeholder="0"/></td>
+              <td><input class="purchaseCost border-0" type="number" placeholder="0"/></td>
+          </tr>
+      </table>
     </div>
   )
 }
